130704598e50d1b0db698da4a7da48b3d2701feb
Bernardo Maiorano <54644125+MaioranoB@users.noreply.github.com>
2020-10-29 14:37:08 -0300

    clean do clean

--- BCD_Ops/somaBCD.vhdl
+++ BCD_Ops/somaBCD.vhdl
@@ -26,18 +26,21 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 
 entity somaBCD is
-port (a,b      : in  std_logic_vector(15 downto 0); --2 numeros de ate 4 algarismos (4*4 = 16bits)
-		sum      : out std_logic_vector(19 downto 0) --maior resultado possivel: 9999+9999 = 19.998 (5*4 = 20bits)
+port (--a,b      : in  std_logic_vector(15 downto 0); --2 numeros de ate 4 algarismos (4*4 = 16bits)
+		--sum      : out std_logic_vector(19 downto 0) --maior resultado possivel: 9999+9999 = 19.998 (5*4 = 20bits)
+		a3,a2,a1,a0 : in  std_logic_vector(3 downto 0);
+		b3,b2,b1,b0 : in  std_logic_vector(3 downto 0);
+		sum4,sum3,sum2,sum1,sum0 : out std_logic_vector(3 downto 0)
 );
 end somaBCD;
 
 architecture structure of somaBCD is
 	
-	type BCDarray  is array (3 downto 0) of std_logic_vector(3 downto 0);
-
-	signal arrayA   : BCDarray := (a(15 downto 12),a(11 downto 8),a(7 downto 4),a(3 downto 0));
-	signal arrayB   : BCDarray := (b(15 downto 12),b(11 downto 8),b(7 downto 4),b(3 downto 0));
-	signal arraySUM : BCDarray;
+--	type BCDarray  is array (3 downto 0) of std_logic_vector(3 downto 0);
+--
+--	signal arrayA   : BCDarray := (a(15 downto 12),a(11 downto 8),a(7 downto 4),a(3 downto 0));
+--	signal arrayB   : BCDarray := (b(15 downto 12),b(11 downto 8),b(7 downto 4),b(3 downto 0));
+--	signal arraySUM : BCDarray;
 	
 	signal c_aux1,c_aux2,c_aux3,c_aux4 : std_logic; --cary's das somas parciais
 	
@@ -50,12 +53,13 @@ architecture structure of somaBCD is
 	end component;
 	
 	begin
-	S0:somaBCD_1digito port map(arrayA(0), arrayB(0), arraySUM(0), '0', c_aux1);
-	S1:somaBCD_1digito port map(arrayA(1), arrayB(1), arraySUM(1), c_aux1, c_aux2);
-	S2:somaBCD_1digito port map(arrayA(2), arrayB(2), arraySUM(2), c_aux2, c_aux3);
-	S3:somaBCD_1digito port map(arrayA(3), arrayB(3), arraySUM(3), c_aux3, c_aux4);
+	S0:somaBCD_1digito port map(a0, b0, sum0, '0', c_aux1);
+	S1:somaBCD_1digito port map(a1, b1, sum1, c_aux1, c_aux2);
+	S2:somaBCD_1digito port map(a2, b2, sum2, c_aux2, c_aux3);
+	S3:somaBCD_1digito port map(a3, b3, sum3, c_aux3, c_aux4);
 	
-	sum <= ("000" & c_aux4) & arraySUM(3) & arraySUM(2) & arraySUM(1) & arraySUM(0);
+	sum4 <="000" & c_aux4;
+	--sum <= ("000" & c_aux4) & arraySUM(3) & arraySUM(2) & arraySUM(1) & arraySUM(0);
 end structure;
 
 --library ieee;
